3423e5468b0d9ae92ed80e8310edf72122de6fd8
kanojadav <150222436+kanojadav@users.noreply.github.com>
2024-03-15 10:21:03 +0530

    variable file

---
+++ VARIABLE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int a=12,b=6,c;
+void main(){
+clrscr();
+printf("variable declation");
+printf("\n addition %d",a+b);
+printf("\n 12+6= %d",a+b);
+printf("\n subtrsction %d",a-b);
+printf("\n12-6 = %d",a-b);
+printf("\nmultiplication %d",a*b);
+printf("\n12*6= %d",a*b);
+printf("\ndivision %d",a/b);
+printf("\n12/6 = %d",a/b);
+
+getch();
+}
