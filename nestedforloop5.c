93629471f5f250dfdfbc353d686673ad298550c9
kanojadav <150222436+kanojadav@users.noreply.github.com>
2024-04-16 12:09:59 +0530

    Add files via upload

---
+++ nestedforloop5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+
+    
+    for (int i = 1; i <= rows; i++) {
+        int num = i;
+        
+        for (int j = 1; j <= rows - i + 1; j++) {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
