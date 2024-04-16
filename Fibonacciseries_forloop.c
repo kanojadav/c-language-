5bbb36258245d3e5975630773819b2954b738105
kanojadav <150222436+kanojadav@users.noreply.github.com>
2024-04-16 12:21:18 +0530

    Add files via upload

---
+++ Fibonacciseries_forloop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm;
+
+    
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    
+    printf("%d, %d, ", t1, t2);
+
+    
+    for (int i = 3; i <= n; i++) {
+        nextTerm = t1 + t2;
+        printf("\n %d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
